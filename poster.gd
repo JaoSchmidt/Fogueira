4ade57a17360796af20f655175ca8552c1726aca
JaoSchmidt <jhsc98@gmail.com>
2023-07-24 04:26:38 -0300

    add initial zoomable object

--- poster.gd
+++ poster.gd
@@ -1,18 +1,6 @@
 extends Area2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-	#self.connect("mouse_entered",self,"onMouseOverlap")
-	#set_process_input(true)
-
-func _input(event):
-	pass
-	#if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-	#	print("Funcionou!")
-	#	hide()
-
+# https://ask.godotengine.org/22606/how-ive-handled-area2d-mouse-click-not-question
 func _on_input_event(viewport, event, shape_idx):
 	if (event is InputEventMouseButton && event.pressed):
 		print("Clicked")
